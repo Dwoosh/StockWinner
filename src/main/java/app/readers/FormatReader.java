661a65fcfc9fd629d31780dcf922c455d3a492f2
jcieloch <38323902+jcieloch@users.noreply.github.com>
2018-12-18 23:38:29 +0100

    Update FormatReader.java

--- src/main/java/app/readers/FormatReader.java
+++ src/main/java/app/readers/FormatReader.java
@@ -1,21 +1,10 @@
 package app.readers;
 
+import app.exceptions.InvalidContentException;
 import app.model.DataPointList;
 
+
 public abstract class FormatReader {
-    protected String path;
 
-    public String getPath(){
-        return this.path;
-    }
-
-    public void setPath(String path){
-        this.path = path;
-    }
-
-    public FormatReader(String path){
-        this.path = path;
-    }
-
-    public abstract DataPointList getDataPointList();
+    public abstract DataPointList getDataPointList(String path) throws InvalidContentException;
 }
