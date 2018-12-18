9a432392451b75803c564684a49dfd207530ac37
Mateusz Monasterski <mateusz.mona@interia.pl>
2018-12-18 22:22:02 +0100

    gui in progress

--- src/main/java/app/controllers/GraphDialogController.java
+++ src/main/java/app/controllers/GraphDialogController.java
@@ -3,12 +3,16 @@ package app.controllers;
 import app.model.*;
 import app.readers.*;
 import app.support.ZoomManager;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
@@ -26,9 +30,33 @@ public class GraphDialogController {
     @FXML
     private LineChart<String, Number> lineChart;
 
+    @FXML
+    private ComboBox dateFrom;
+
+    @FXML
+    private ComboBox dateTo;
+
+    @FXML
+    private ComboBox<StrategyEnums.Actions> action;
+
+    @FXML
+    private ComboBox<StrategyEnums.Conditions> condition;
+
+    @FXML
+    private TextField percent;
+
+    @FXML
+    private ListView<String> condList;
+
     public void initialize(AppController controller, String fileLocation, WebSites.SupportedWebSites chosenWebsite, Pane chartParent) throws IOException {
 
         this.appController = controller;
+        this.action.setItems(FXCollections.observableArrayList(StrategyEnums.Actions.values()));
+        //this.condition.setItems(FXCollections.observableArrayList(StrategyEnums.Conditions.values()));
+        this.condition.getItems().addAll(
+            StrategyEnums.Conditions.AND,
+                StrategyEnums.Conditions.OR
+        );
 
         xAxis.setLabel("Date");
         yAxis.setLabel("Price");
@@ -42,6 +70,8 @@ public class GraphDialogController {
 
         for(DataPoint dp: pointList.getDataPoints()) {
             series.getData().add(new XYChart.Data(dp.getDate().toString(), dp.getPrice()));
+            this.dateFrom.getItems().add(dp.getDate().toString());
+            this.dateTo.getItems().add(dp.getDate().toString());
         }
         new ZoomManager(chartParent, lineChart,series);
     }
@@ -57,6 +87,31 @@ public class GraphDialogController {
     private void handleReset(ActionEvent event){
 
         appController.initGraphScene("views/GraphView.fxml");
+    }
+    @FXML
+    private void handleAddCondButton(ActionEvent event){
+
+        String cond;
+        if(condition.getSelectionModel().isEmpty()){
+            cond = "";
+            condition.setDisable(false);
+
+        }
+        else {
+            cond = condition.getValue().toString();
+            condition.setDisable(true);
+        }
+
+
+        condList.getItems().add(cond +" From " + dateFrom.getValue()
+                + " to " + dateTo.getValue() + " percent change " + percent.getText()
+                + " action " + action.getValue().toString());
+    }
+
+    @FXML
+    private void handleDoneButton(ActionEvent event){
+
+
     }
 
 }
